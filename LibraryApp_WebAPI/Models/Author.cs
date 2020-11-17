627fcd7ed5d892a1daea6981508516ee62755203
Vajk <vajkone18@gmail.com>
2020-11-17 23:11:31 +0100

    Added Author model class

---
+++ LibraryApp_WebAPI/Models/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApp_WebAPI.Models
+{
+    public class Author
+    {
+        [Key]
+        public int AuthorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
