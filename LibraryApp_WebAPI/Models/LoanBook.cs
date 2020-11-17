c22a093cb9b2a2d8cc5a0765136b18e437796acc
Vajk <vajkone18@gmail.com>
2020-11-17 23:28:43 +0100

    Added LoanBook Model class

---
+++ LibraryApp_WebAPI/Models/LoanBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApp_WebAPI.Models
+{
+    public class LoanBook
+    {
+        [ForeignKey("Member")]
+        public int MemberId { get; set; }
+        public Member Member { get; set; }
+
+        [ForeignKey("LibraryBook")]
+        public string InventoryNumber { get; set; }
+        public LibraryBook LibraryBook { get; set; }
+
+        public DateTime LoanDate { get; set; }
+        
+        public DateTime returnDate { get; set; }
+    }
+}
