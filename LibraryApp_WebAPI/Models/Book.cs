af6780496ab666e3feb88a71c66d20e9dc9edce5
Vajk <vajkone18@gmail.com>
2020-11-17 23:09:47 +0100

    Added Book model class

---
+++ LibraryApp_WebAPI/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApp_WebAPI.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Pages { get; set; }
+
+    }
+}
--- LibraryApp_WebAPI/Models/Member.cs
+++ LibraryApp_WebAPI/Models/Member.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace LibraryApp_WebAPI.Models
 {
     public class Member
     {
+        [Key]
         public int MemberId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
