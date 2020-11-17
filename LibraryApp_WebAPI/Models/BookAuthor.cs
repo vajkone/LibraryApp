6125b65c831c4a93b47177466e531b3857e99411
Vajk <vajkone18@gmail.com>
2020-11-17 23:19:28 +0100

    Added BookAuthor model class

---
+++ LibraryApp_WebAPI/Models/BookAuthor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApp_WebAPI.Models
+{
+    public class BookAuthor
+    {
+        
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
+        public Author author { get; set; }
+
+
+        
+        [ForeignKey("Book")]
+        public int BookId { get; set; }
+        public Book book { get; set; }
+
+
+    }
+}
